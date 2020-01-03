89f58f1c0a6bd88fc3213b12884a4afaf70ee514
Alvaro Zamora <alvarozamora@stanford.edu>
2020-01-02 21:17:02 -0800

    Setup Progress

--- resnet.py
+++ resnet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 import torchvision
 from LSUV import LSUVinit
 
@@ -47,56 +48,16 @@ if __name__ == '__main__':
 			model.iter += 1
 			pred = model.forward(I)
 
-mse = ((pred-I)**2).sum(-1).sum(-1).sum(-1)
-hl = 100*model.HLoss(pred,I)
-kl = torch.max(10*torch.ones_like(model.KLoss), model.KLoss)
+			loss = (hl + mse + kl + adS + bowl).mean()
+			loss.backward()
 
-channels = 3
-grad_x_weights = torch.tensor([[1, -1]], dtype=torch.float32).to(model.device)
-grad_x_weights = grad_x_weights.expand(channels, 1, 1, 2)
-Gx = F.conv2d(I, grad_x_weights, groups=I.shape[1], padding=1)
-gx = F.conv2d(pred, grad_x_weights, groups=pred.shape[1], padding=1)
-grad_y_weights = torch.tensor([[ 1],[ -1]], dtype=torch.float32).to(model.device)
-grad_y_weights = grad_y_weights.expand(channels, 1, 2, 1)
-Gy = F.conv2d(I, grad_y_weights, groups=I.shape[1], padding=1)
-gy = F.conv2d(pred, grad_y_weights, groups=pred.shape[1], padding=1)
+			print("Epoch", epoch, "batch", q, "loss", np.round(loss.item(),4), "kl", np.round(kl.mean().item(),4), "mse", np.round(mse.mean().item(),4), "img means", "({}, {})".format(np.round(pred.mean().item(),3), np.round(I.mean().item(),3)))
 
+			if model.iter%100 == 0:
+				losses.append([loss.item()])
 
-adS = torch.zeros_like(mse)#1.0*(((Gx-gx)**2).sum(-1).sum(-1).sum(-1) + ((Gy-gy)**2).sum(-1).sum(-1).sum(-1))
-bowl = (F.relu(I-1) + F.relu(-I)).sum(-1).sum(-1).sum(-1)
-loss = (hl + mse + kl + adS + bowl).mean()
-print("Epoch", epoch, "batch", q, "loss", np.round(loss.item(),4), "kl", np.round(kl.mean().item(),4), "mse", np.round(mse.mean().item(),4), "img means", "({}, {})".format(np.round(pred.mean().item(),3), np.round(I.mean().item(),3)))
-#print("means", model.gaussian_parameters(model.prior)[0].min().item(),  model.gaussian_parameters(model.prior)[0].max().item(), "std", "({} {})".format(np.round(pred.reshape(pred.size(0),pred.size(1),-1).std(dim=-1).mean().item(), 3), np.round(I.reshape(I.size(0), I.size(1),-1).std(dim=-1).mean().item(), 3)), "DSSIM", hl.mean().item(), "grads", adS.mean().item())
-print("std", "({} {})".format(np.round(pred.reshape(pred.size(0),pred.size(1),-1).std(dim=-1).mean().item(), 3), np.round(I.reshape(I.size(0), I.size(1),-1).std(dim=-1).mean().item(), 3)), "DSSIM", hl.mean().item(), "grads", adS.mean().item())
-if model.iter%100 == 0:
-    losses.append([kl.mean().item(), mse.mean().item(), adS.mean().item(), hl.mean().item()])
-loss.backward()
-optimizer.step()
-optimizer.zero_grad()
+			optimizer.step()
+			optimizer.zero_grad()
 
-if (q + epoch)%50 == 1:
-    img = torch.cat((pred[:8],I[:8]),dim=0)
-    nrow = I.size(0)
-    nrow = 8
-    torchvision.utils.save_image(img, 'gen.png', nrow=nrow, padding=1)
-
-#if (epoch+1)%2==0 and q == 0:
-if model.iter%500 == 0:
-    img = torch.cat((pred[:8],I[:8]),dim=0)
-    nrow = I.size(0)
-    nrow = 8
-    torchvision.utils.save_image(img, 'gen_'+str(epoch+1)+"_"+str(model.iter)+'.png', nrow=nrow, padding=1)
-    np.save('Loss.npy', np.array(losses))
-
-#if model.iter == 500:
-#    torch.save(optimizer.state_dict(), 'Optim_iter_'+str(model.iter)+'.pth')
-
-    #if ((epoch+1)%1)==0:
-      #torch.save(model.state_dict(), 'Model_'+str(epoch+1)+'.pth')
-    #  torch.save(model, 'Model_all_'+str(epoch+1)+'.pth')
-    #  torch.save(optimizer.state_dict(), 'Optim_'+str(epoch+1)+'.pth')
-    #  torch.save(model.prior, 'Model_prior_'+str(epoch+1)+'.pth')
-  #torch.save(model.state_dict(), 'Model.pth')
-  torch.save(model, 'Model_all.pth')
-
-  np.save('Loss.npy', np.array(losses))
+		torch.save(model, f'Model{epoch:04d}.pth')
+		np.save('Loss.npy', np.array(losses))
